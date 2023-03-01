29e56d55e8b3802413f1d4b21b8116dd36067a7b
skysyzygy <solson@bam.org>
2023-03-01 15:11:47 -0500

    Basic p2_orphan work, pending completion of audit -> stream function

--- R/p2_orphan.R
+++ R/p2_orphan.R
@@ -6,11 +6,16 @@
 #'
 #' @param since date after which we look for changed emails
 #'
-#' @return data.table of changed emails with columns `from`, `to`, and `customer_no`
+#' @return data.table of changed emails with columns `old_value`, `new_value`, and `customer_no`
 #' @importFrom tessilake read_tessi
 tessi_changed_emails <- function(since = Sys.Date() - 7) {
 
-    a <- read_tessi("audit")
+    a <- stream_load_audit("T_EADDRESS") %>%
+      .[(is.na(column_updated) | column_updated %in% c("primary_ind","address")) & !is.na(customer_no)]
+
+    setnafill(a,"locf",cols=c("new_value","old_value"),by = "key_no")
+    a[]
+
 }
 
 p2_update_email <- function(from = NULL, to = NULL, customer_no = NULL) {
